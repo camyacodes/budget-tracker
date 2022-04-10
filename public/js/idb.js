860ae8c7bb8cd00a4c0a73b0bf1c247525481d45
Camya <camyacodes@gmail.com>
2022-04-10 13:33:26 -0400

    setup index db connection

---
+++ public/js/idb.js
@@ -0,0 +1,21 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = (event) => {
+  event.target.result.createObjectStore("pending", {
+    keyPath: "id",
+    autoIncrement: true
+  });
+};
+
+request.onerror = (err) => {
+  console.log(err.message);
+};
+
+request.onsuccess = (event) => {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
