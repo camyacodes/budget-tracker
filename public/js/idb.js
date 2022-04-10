61a70682a184136a8a33ed98071eadfcd5dc96dc
Camya <camyacodes@gmail.com>
2022-04-10 13:52:29 -0400

    indexdb functionality working

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,21 +1,38 @@
+// create variable to hold db connection
 let db;
-const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = (event) => {
-  event.target.result.createObjectStore("pending", {
-    keyPath: "id",
-    autoIncrement: true
-  });
+// establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes
+request.onupgradeneeded = function(event) {
+
+    const db = event.target.result;
+
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
-request.onerror = (err) => {
-  console.log(err.message);
+// upon a successful
+request.onsuccess = function(event) {
+
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        // todo: uploadTransaction();
+    }
 };
 
-request.onsuccess = (event) => {
-  db = event.target.result;
-
-  if (navigator.onLine) {
-    checkDatabase();
-  }
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
 };
+
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const  budgetObjectStore = transaction.objectStore('new_transaction');
+
+    budgetObjectStore.add(record);
+}
